46e705d7eda79acea4f3c1f89fc8e40b46d1f942
Sapa Denys <sapa.denis@gmail.com>
2014-09-29 00:28:54 +0300

    Initial Commit

---
+++ DES_3DES_Algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DES_3DES_Algorithms
+//
+//  Created by Sapa Denys on 29.09.14.
+//  Copyright (c) 2014 Sapa Denys. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
