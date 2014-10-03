7acdc550270e96508f5af5586fc612c32b82fffc
Sapa Denys <sapa.denis@gmail.com>
2014-10-03 11:48:48 +0300

    init

--- DES_3DES_Algorithms/main.cpp
+++ DES_3DES_Algorithms/main.cpp
@@ -7,9 +7,40 @@
 //
 
 #include <iostream>
+#include <string>
+#include <bitset>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+using namespace std;
+
+
+int main(int argc, const char * argv[])
+{
+	string inputString = "Hello, World!";
+//	bitset<64> bitMask (inputString);
+	
+	long long foo = inputString[1];
+
+	cout << bitset<64>(foo) << endl << CHAR_BIT << endl;
+	
     return 0;
+}
+
+
+bitset<64> stringToBitset(char *data)
+{
+	bitset<64> resultBitset;
+	
+	int numberOfBytes = 64 / CHAR_BIT;
+	
+	for (int i = 0; i < numberOfBytes; ++i) {
+		int offset = i *CHAR_BIT;
+		bitset<CHAR_BIT> symbol (data[i]);
+		
+//		for (; symbol; symbol >>=symbol) {
+//			<#statements#>
+//		}
+		
+	}
+	
+	return 0;
 }
