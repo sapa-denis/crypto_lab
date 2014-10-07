39a320cb1da514698bcfcbd5d6f1d9d6c93f5a2b
Sapa Denys <sapa.denis@gmail.com>
2014-10-07 23:41:37 +0300

    ~

--- DES_3DES_Algorithms/main.cpp
+++ DES_3DES_Algorithms/main.cpp
@@ -13,19 +13,6 @@
 using namespace std;
 
 
-int main(int argc, const char * argv[])
-{
-	string inputString = "Hello, World!";
-//	bitset<64> bitMask (inputString);
-	
-	long long foo = inputString[1];
-
-	cout << bitset<64>(foo) << endl << CHAR_BIT << endl;
-	
-    return 0;
-}
-
-
 bitset<64> stringToBitset(char *data)
 {
 	bitset<64> resultBitset;
@@ -34,13 +21,52 @@ bitset<64> stringToBitset(char *data)
 	
 	for (int i = 0; i < numberOfBytes; ++i) {
 		int offset = i *CHAR_BIT;
-		bitset<CHAR_BIT> symbol (data[i]);
+		bitset<CHAR_BIT> symbol = (long)data[i];
 		
-//		for (; symbol; symbol >>=symbol) {
-//			<#statements#>
-//		}
+		for (int j = 0; j < offset; ++j, ++offset) {
+			resultBitset[offset] = symbol[j];
+		}
 		
 	}
 	
-	return 0;
+	return resultBitset;
 }
+
+int main(int argc, const char * argv[])
+{
+	string inputString = "Hi, Ju!!";
+//	bitset<64> bitMask (inputString);
+	
+	long long foo = inputString[0];
+
+	cout << inputString.length() << endl;
+	
+	
+	// превращаем 8 символов в последовательность битов. Для хранения используется long long
+	foo <<= 8;
+	foo |= inputString[1];
+	foo <<= 8;
+	foo |= inputString[2];
+	foo <<= 8;
+	foo |= inputString[3];
+	foo <<= 8;
+	foo |= inputString[4];
+	foo <<= 8;
+	foo |= inputString[5];
+	foo <<= 8;
+	foo |= inputString[6];
+	foo <<= 8;
+	foo |= inputString[7];
+	
+	cout << bitset<64>(foo) << endl;
+	int r = foo;
+//	cout << bitset<32>(r) << endl;
+	int l = foo >>= 32;
+	cout << bitset<32>(l) << endl << bitset<32>(r) << endl;
+	
+	
+	
+    return 0;
+}
+
+
