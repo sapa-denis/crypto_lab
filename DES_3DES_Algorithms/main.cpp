cae277b7e0aae61ce274953a68a721a2b365e98a
Sapa Denys <sapa.denis@gmail.com>
2014-10-27 23:25:16 +0200

    add weak key chacker

--- DES_3DES_Algorithms/main.cpp
+++ DES_3DES_Algorithms/main.cpp
@@ -58,48 +58,55 @@ void swapBits(long long &source, const short &first, const short &second )
 
 }
 
+bool chackWeakKey(const long& key)
+{
+	return !(	(key - 0x0101010101010101)
+			 && (key - 0xFEFEFEFEFEFEFEFE)
+			 && (key - 0x1F1F1F1F1F1F1F1F)
+			 && (key - 0xE0E0E0E0E0E0E0E0)
+			 );
+}
+
 int main(int argc, const char * argv[])
 {
 	string inputString = "Hi, Jul!";
-//	bitset<64> bitMask (inputString);
 	
-	long long foo = inputString[0];
-
-//	cout << inputString.length() << endl;
+	long long decimalStringPresentation = inputString[0];
 	
 	
 	// превращаем 8 символов в последовательность битов. Для хранения используется long long
-	foo <<= 8;
-	foo |= inputString[1];
-	foo <<= 8;
-	foo |= inputString[2];
-	foo <<= 8;
-	foo |= inputString[3];
-	foo <<= 8;
-	foo |= inputString[4];
-	foo <<= 8;
-	foo |= inputString[5];
-	foo <<= 8;
-	foo |= inputString[6];
-	foo <<= 8;
-	foo |= inputString[7];
+	decimalStringPresentation <<= 8;
+	decimalStringPresentation |= inputString[1];
+	decimalStringPresentation <<= 8;
+	decimalStringPresentation |= inputString[2];
+	decimalStringPresentation <<= 8;
+	decimalStringPresentation |= inputString[3];
+	decimalStringPresentation <<= 8;
+	decimalStringPresentation |= inputString[4];
+	decimalStringPresentation <<= 8;
+	decimalStringPresentation |= inputString[5];
+	decimalStringPresentation <<= 8;
+	decimalStringPresentation |= inputString[6];
+	decimalStringPresentation <<= 8;
+	decimalStringPresentation |= inputString[7];
 	
-	cout << bitset<64>(foo) << "++" << endl;
-	int r = foo;
+	cout << bitset<64>(decimalStringPresentation) << "++" << endl;
+
+	//Начальная перестановка, матрица IP. Всего 64 вызова
+	//TODO: дописать остальные 63 вызова перестановки.
+	swapBits(decimalStringPresentation, 0, 58);
 	
 	
-	
-	swapBits(foo, 1, 5);
-	
-	std::cout << std::bitset<64>(foo) << "~~~" << std::endl;
-	
-//	cout << bitset<32>(r) << endl;
-	int l = foo >>= 32;
-	cout << bitset<32>(l) << endl << bitset<32>(r) << endl;
+	//Разделение на левые и правые полуслова по 32 бита.
+	int rigthPart = decimalStringPresentation;
+	int leftPart = decimalStringPresentation >>= 32;
+	cout << bitset<32>(leftPart) << endl << bitset<32>(rigthPart) << endl;
 	
 	
 	
     return 0;
 }
+
+
 
 
